762f883c2002ca59c20fc4e5732e7e2da7b305e5
joallisson <joallissonnc.com>
2022-07-23 15:04:17 -0300

    98. Animação dos inimigos - Parte 2

--- Projeto02-AntSmash/Assets/_Game/Scripts/Enemy.cs
+++ Projeto02-AntSmash/Assets/_Game/Scripts/Enemy.cs
@@ -5,20 +5,26 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float speed;
+    private Animator myAnimator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Movement();
+        AnimationSpeed();
     }
 
     private void Movement()
     {
         transform.Translate(Vector2.down * (speed * Time.deltaTime));
+    }
+
+    private void AnimationSpeed(){
+        myAnimator.SetFloat("Speed", speed); //alterando o valor da variavel definida na janela do animator
     }
 }
