4d2b05000b55444d318b9ddc9ab31957d13242c3
joallisson <joallissonnc.com>
2022-07-24 18:17:23 -0300

    108. Menu Pause (Programação) - Parte 2

--- Projeto02-AntSmash/Assets/_Game/Scripts/UIController.cs
+++ Projeto02-AntSmash/Assets/_Game/Scripts/UIController.cs
@@ -8,10 +8,12 @@ public class UIController : MonoBehaviour
 {
     public TMP_Text txtScore; //variavel que guarda a pontuação
     public Image[] imageLifes; //criando array de vidas do jogador
+    public GameObject panelGame, panelPause;
     // Start is called before the first frame update
     void Start()
     {
-        
+        panelGame.gameObject.SetActive(true); //ativando o painel do jogo
+        panelPause.gameObject.SetActive(false); //desativando o painel de pause
     }
 
     // Update is called once per frame
@@ -22,6 +24,28 @@ public class UIController : MonoBehaviour
 
     public void UpdateScore(int score)
     {
-        txtScore.text = score.ToString(); //converte a pontuação em string 
+        txtScore.text = score.ToString(); //converte a pontuação em string
     }
+
+    public void ButtonPause()
+    {
+        Time.timeScale = 0f; //pausando o game
+        panelGame.gameObject.SetActive(false); //desativando o painel do jogo
+        panelPause.gameObject.SetActive(true); //ativando o painel de pause
+    }
+
+    public void ButtonResume()
+    {
+        Time.timeScale = 1f; //Saindo do pause
+        panelGame.gameObject.SetActive(true);  //ativando o painel do jogo 
+        panelPause.gameObject.SetActive(false); //desativando o painel de pause
+    }
+
+    public void ButtonBackMainMenu() //voltando para o menu principal
+    {
+        Time.timeScale = 1f; //Saindo do pause
+        panelGame.gameObject.SetActive(false);
+        panelPause.gameObject.SetActive(false);
+    }
+
 }
